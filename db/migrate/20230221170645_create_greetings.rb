66da1770739f87de54631db10415c4d40dca4bb0
d4nQw3rty <danlinmoy@gmail.com>
2023-02-22 11:44:29 -0400

    update

--- db/migrate/20230221170645_create_greetings.rb
+++ db/migrate/20230221170645_create_greetings.rb
@@ -1,7 +1,7 @@
 class CreateGreetings < ActiveRecord::Migration[7.0]
   def change
     create_table :greetings do |t|
-      t.string :greeting
+      t.string :message
 
       t.timestamps
     end
