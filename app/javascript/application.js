93185815b270d559a0952c3f49955e09d3737776
d4nQw3rty <danlinmoy@gmail.com>
2023-02-22 11:45:35 -0400

    update application

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,16 +1,15 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
+import App from './App';
 
-function App() {
-  return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
-  );
-}
-
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root'),
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+</Provider>,
 );
